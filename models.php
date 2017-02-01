a8d348ba66b28eab311125e292447dad62555fcf
Kevin Keller <flygeneticist@gmail.com>
2017-02-01 14:11:09 +0800

    Adds asset class to models

--- models.php
+++ models.php
@@ -5,4 +5,6 @@ class User extends Illuminate\Database\Eloquent\Model {}
 
 class Playground extends Illuminate\Database\Eloquent\Model {}
 
+class Asset extends Illuminate\Database\Eloquent\Model {}
+
 ?>
